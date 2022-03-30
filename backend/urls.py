34389d2d2f74e056ae3f8a8ec263abe0435a1adf
Yoann Janssens <74479083+y-janssens@users.noreply.github.com>
2022-03-30 23:21:24 +0200

    update

--- backend/urls.py
+++ backend/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    #path('', TemplateView.as_view(template_name="index.html")),
-    path('api/', include('api.urls')),
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
+    path('api/', include('api.urls')),    
 ]
