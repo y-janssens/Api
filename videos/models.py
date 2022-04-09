14e0454727c26e9c684479b8895a4e270f09c9ab
Yoann Janssens <74479083+y-janssens@users.noreply.github.com>
2022-04-10 00:56:32 +0200

    test

--- videos/models.py
+++ videos/models.py
@@ -10,9 +10,10 @@ class Video(models.Model):
     name = models.CharField(max_length=1500, blank=True, null=True)
     author = models.CharField(max_length=250, blank=True, null=True)
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
-    thumbnail = models.FileField(blank=False, null=False)
-    file = models.FileField(upload_to=('videos'), blank=False, null=False)
+    #thumbnail = models.FileField(blank=False, null=False)
+    #file = models.FileField(upload_to=('videos'), blank=False, null=False)
     fileName = models.CharField(max_length=1500, blank=True, null=True)
+    thumbnailName = models.CharField(max_length=1500, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def __str__(self):
