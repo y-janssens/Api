f42e2f3b5d9aad1723090559ea2dd69ac0f57474
Yoann Janssens <74479083+y-janssens@users.noreply.github.com>
2022-04-09 18:33:18 +0200

    update

--- videos/models.py
+++ videos/models.py
@@ -7,12 +7,12 @@ class Category(models.Model):
         return self.name
 
 class Video(models.Model):
-    name = models.CharField(max_length=150, blank=True, null=True)
-    author = models.CharField(max_length=50, blank=True, null=True)
+    name = models.CharField(max_length=1500, blank=True, null=True)
+    author = models.CharField(max_length=250, blank=True, null=True)
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
     thumbnail = models.FileField(blank=False, null=False)
     file = models.FileField(upload_to=('videos'), blank=False, null=False)
-    fileName = models.CharField(max_length=150, blank=True, null=True)
+    fileName = models.CharField(max_length=1500, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def __str__(self):
